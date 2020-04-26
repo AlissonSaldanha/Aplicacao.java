22f19f64c7ab53c5e7023227c432ee6216a35606
AlissonSaldanha <alisson.saldanha@outlook.com.br>
2020-04-26 16:55:23 -0300

    Projeto criado

---
+++ src/main/java/com/alissonweb/aplicacaojava/AplicacaoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.alissonweb.aplicacaojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacaoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacaoJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alissonweb/aplicacaojava/AplicacaoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alissonweb.aplicacaojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacaoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
